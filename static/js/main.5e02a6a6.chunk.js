2b551637d3475554372397c7ddce67440e0375e9
montheramer <monther.amer48@gmail.com>
2020-10-28 20:09:38 +0300

    Updates

---
+++ static/js/main.5e02a6a6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpprofile=this.webpackJsonpprofile||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n(1),i=n.n(r),s=n(3),o=n.n(s);n(9);var l=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("h1",{children:"hello"})})};o.a.render(Object(c.jsx)(i.a.StrictMode,{children:Object(c.jsx)(l,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.5e02a6a6.chunk.js.map
