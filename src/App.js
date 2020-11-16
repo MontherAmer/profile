841914badf092d89bd8fd1ad1fff3be99a154d9d
montheramer <monther.amer48@gmail.com>
2020-11-16 23:11:08 +0200

    HANDLE animation on sidebar hover

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import React, { useState } from 'react';
+
+import SideBar from './components/SideBar';
+import Main from './components/Main';
+
+export default () => {
+  const [state, setState] = useState({});
+  const handleHover = (e) => setState({ ...state, shrink: e });
   return (
     <div className="App">
-      <h1>hello</h1>
+      <SideBar shrink={state.shrink} handleHover={handleHover} />
+      <Main shrink={state.shrink} />
     </div>
   );
-}
-
-export default App;
+};
